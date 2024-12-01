1cb47c8e9453f4e2d2c622a7ee4142c1df0e6a7c
Wiltort <wiltort21@gmail.com>
2024-12-01 13:10:16 +0500

    activated_invite_code добавлен в сериалайзер

--- referal_app/users/serializers.py
+++ referal_app/users/serializers.py
@@ -11,12 +11,18 @@ class ReferralSerializer(serializers.ModelSerializer):
 class UserProfileSerializer(serializers.ModelSerializer):
     referrals = ReferralSerializer(many=True, read_only=True)
     referred_by = ReferralSerializer(read_only=True)
+    activated_invite_code = serializers.SerializerMethodField()
 
 
     class Meta:
         model = UserProfile
-        fields = ["phone_number", "invite_code", "referred_by", "referrals"]
+        fields = ["phone_number", "invite_code", "referred_by", "referrals", "activated_invite_code"]
         extra_kwargs = {
             'phone_number': {'required': False},
             'invite_code': {'required': False},
         }
+
+    def get_activated_invite_code(self, obj):
+        if obj.referred_by:
+            return obj.referred_by.invite_code
+        return None
